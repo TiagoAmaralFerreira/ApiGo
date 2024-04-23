b68c2e710bd60f97bb933390d4370bdb65a7ee4e
Tiago Amaral Ferreira <82990234+TiagoAmaralFerreira@users.noreply.github.com>
2024-04-23 08:07:13 -0300

    feat: Criação da query para SELECT

---
+++ models/get.go
@@ -0,0 +1,16 @@
+package models
+
+import "crudPostgreSQL/db"
+
+func Get(id int64) (todo Todo, err error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	row := conn.QueryRow(`SELECT * FROM todos WHERE id=$1`, id)
+	err = row.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Done)
+
+	return
+}
