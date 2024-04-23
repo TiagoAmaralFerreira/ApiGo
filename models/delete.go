d3971fa15a6bef45a1fc89bb737ec0b98463d292
Tiago Amaral Ferreira <82990234+TiagoAmaralFerreira@users.noreply.github.com>
2024-04-23 12:37:06 -0300

    feat: Criação da query para deletar.

--- models/delete.go
+++ models/delete.go
@@ -2,14 +2,14 @@ package models
 
 import "crudPostgreSQL/db"
 
-func Update(id int64, todo Todo) (int64, error) {
+func Delete(id int64) (int64, error) {
 	conn, err := db.OpenConnection()
 	if err != nil {
 		return 0, err
 	}
 	defer conn.Close()
 
-	res, err := conn.Exec(`UPDATE todos SET title=$2, description=$3, done=$5 WHERE id=$1`, id, todo.Title, todo.Description, todo.Done)
+	res, err := conn.Exec(`DELETE FROM todos WHERE id=$1`, id)
 	if err != nil {
 		return 0, err
 	}
